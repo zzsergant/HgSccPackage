95cfa9120d3d32f7cb69e51ec841693228e9b599
Sergey Antonov <sergant_@mail.ru>
2009-05-27 22:37:04 +0400

    Поменял сохранение опций с сериализации XML на запись в реестр HKCU. Достали проблемы с вспомогательными Dll для сериализации.

--- HgSccPackage/HgSccHelper/HgSccOptions.cs
+++ HgSccPackage/HgSccHelper/HgSccOptions.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using System.Windows.Forms;
-using System.Xml.Serialization;
+using Microsoft.Win32;
 
 //=============================================================================
 namespace HgSccPackage.HgSccHelper
 {
-	[Serializable]
 	public class HgPkgOptions
 	{
 		public string DiffTool { get; set; }
@@ -22,20 +21,23 @@ namespace HgSccPackage.HgSccHelper
 	public sealed class HgSccOptions
 	{
 		static readonly HgSccOptions instance = new HgSccOptions();
+		static readonly string RegKey_DiffTool = "DiffTool";
+
 		HgPkgOptions options;
 
 		// Explicit static constructor to tell C# compiler
 		// not to mark type as beforefieldinit
 		static HgSccOptions()
 		{
+			instance.options = new HgPkgOptions();
 		}
 
 		//-----------------------------------------------------------------------------
 		private HgSccOptions()
 		{
-			options = Load();
-			if (options == null)
-				options = new HgPkgOptions();
+			var new_options = Load();
+			if (new_options != null)
+				options = new_options;
 		}
 
 		//-----------------------------------------------------------------------------
@@ -47,17 +49,11 @@ namespace HgSccPackage.HgSccHelper
 			}
 		}
 
-		//-----------------------------------------------------------------------------
-		private static string CfgPath
+		private static string RegistryPath
 		{
 			get
 			{
-				string appdata = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-				string hgdata = Path.Combine(appdata, "HgSccPackage");
-				if (!Directory.Exists(hgdata))
-					Directory.CreateDirectory(hgdata);
-
-				return Path.Combine(hgdata, "hgsccpkg.xml");
+				return "Software\\Zz\\HgSccPackage";
 			}
 		}
 
@@ -66,20 +62,16 @@ namespace HgSccPackage.HgSccHelper
 		{
 			try
 			{
-				string cfg = CfgPath;
-
-				// Create an instance of the XmlSerializer class;
-				// specify the type of object to serialize.
-				XmlSerializer serializer = new XmlSerializer(Options.GetType());
-				using (var writer = new StreamWriter(cfg))
+				var hg_key = Registry.CurrentUser.CreateSubKey(RegistryPath);
+				if (hg_key != null)
 				{
-					// Serialize the purchase order, and close the TextWriter.
-					serializer.Serialize(writer, Options);
+					hg_key.SetValue(RegKey_DiffTool, Options.DiffTool, RegistryValueKind.String);
+					hg_key.Close();
 				}
 			}
 			catch (System.Exception e)
 			{
-				System.Windows.Forms.MessageBox.Show(e.Message);
+				System.Windows.Forms.MessageBox.Show(e.Message, "Error in Save", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			}
 		}
 
@@ -88,49 +80,21 @@ namespace HgSccPackage.HgSccHelper
 		{
 			try
 			{
-				string cfg = CfgPath;
-				if (File.Exists(cfg))
+				var hg_key = Registry.CurrentUser.OpenSubKey(RegistryPath);
+				if (hg_key != null)
 				{
-					// Create an instance of the XmlSerializer class;
-					// specify the type of object to be deserialized.
-					XmlSerializer serializer = new XmlSerializer(typeof(HgPkgOptions));
-
-					/* If the XML document has been altered with unknown 
-					nodes or attributes, handle them with the 
-					UnknownNode and UnknownAttribute events.*/
-
-					serializer.UnknownNode += new XmlNodeEventHandler(serializer_UnknownNode);
-					serializer.UnknownAttribute += new XmlAttributeEventHandler(serializer_UnknownAttribute);
-
-					using (var fs = new FileStream(cfg, FileMode.Open))
-					{
-						// Declare an object variable of the type to be deserialized.
-						var o = (HgPkgOptions)serializer.Deserialize(fs);
-						if (o != null)
-							return o;
-					}
+					string diff_tool = (string)hg_key.GetValue(RegKey_DiffTool, Options.DiffTool);
+					if (diff_tool != null)
+						Options.DiffTool = diff_tool;
+					hg_key.Close();
 				}
 			}
 			catch (System.Exception e)
 			{
-				System.Windows.Forms.MessageBox.Show(e.Message);
+				System.Windows.Forms.MessageBox.Show(e.Message, "Error in Load", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			}
 
 			return new HgPkgOptions();
-		}
-
-		//-----------------------------------------------------------------------------
-		private static void serializer_UnknownNode(object sender, XmlNodeEventArgs e)
-		{
-//			Console.WriteLine("Unknown Node:" + e.Name + "\t" + e.Text);
-		}
-
-		//-----------------------------------------------------------------------------
-		private static void serializer_UnknownAttribute(object sender, XmlAttributeEventArgs e)
-		{
-// 			System.Xml.XmlAttribute attr = e.Attr;
-// 			Console.WriteLine("Unknown attribute " +
-// 			attr.Name + "='" + attr.Value + "'");
 		}
 	}
 }
