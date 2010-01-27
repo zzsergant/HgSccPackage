a98ea7f2e5b5265bc151a6cc8bf02d627ab53831
Sergey Antonov <sergant_@mail.ru>
2010-01-27 20:03:18 +0300

    Added BooleanToInvertedBoolean converter for using in XAML

--- HgSccHelper/Util.cs
+++ HgSccHelper/Util.cs
@@ -72,4 +72,25 @@ namespace HgSccHelper
 			}
 		}
 	}
+
+	//==================================================================
+	public class BoolToOppositeBoolConverter : System.Windows.Data.IValueConverter
+	{
+		//------------------------------------------------------------------
+		public object Convert(object value, Type targetType, object parameter,
+			System.Globalization.CultureInfo culture)
+		{
+			if (targetType != typeof(bool))
+				throw new InvalidOperationException("The target must be a boolean");
+
+			return !(bool)value;
+		}
+
+		//------------------------------------------------------------------
+		public object ConvertBack(object value, Type targetType, object parameter,
+			System.Globalization.CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+	}
 }
