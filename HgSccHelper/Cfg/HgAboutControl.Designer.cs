a56f03e1bd4b8f3070c7bd7ccb12444f81c1199a
Sergey Antonov <sergant_@mail.ru>
2010-04-22 19:39:21 +0400

    Changed version to v1.3

--- HgSccHelper/Cfg/HgAboutControl.Designer.cs
+++ HgSccHelper/Cfg/HgAboutControl.Designer.cs
@@ -178,7 +178,7 @@
 			this.textBox5.ReadOnly = true;
 			this.textBox5.Size = new System.Drawing.Size(199, 20);
 			this.textBox5.TabIndex = 13;
-			this.textBox5.Text = "HgSccPackage v1.2";
+			this.textBox5.Text = "HgSccPackage v1.3";
 			// 
 			// label7
 			// 
