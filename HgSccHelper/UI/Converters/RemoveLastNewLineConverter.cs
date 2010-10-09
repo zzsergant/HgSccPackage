99a1796249ff556a549f3997c8c4f0fd27accd5c
Sergey Antonov <sergant_@mail.ru>
2010-10-09 12:18:12 +0400

    Removed extra new line(s) at the end of commit message in folowing windows: RevisionGraph, Grep, FileHistory, Annotate

---
+++ HgSccHelper/UI/Converters/RemoveLastNewLineConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace HgSccHelper.UI.Converters
+{
+	//==================================================================
+	public class RemoveLastNewLineConverter : IValueConverter
+	{
+		char[] new_line = new char[] { '\r', '\n' };
+
+		//------------------------------------------------------------------
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value != null)
+			{
+				var trimmed = ((string)value).TrimEnd(new_line);
+				return trimmed;
+			}
+
+			return string.Empty;
+		}
+
+		//------------------------------------------------------------------
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new Exception("The method or operation is not implemented.");
+		}
+	}
+}
