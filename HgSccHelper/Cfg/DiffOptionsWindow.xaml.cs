6de5b16b05aae83a3fd637ef417c505785a8c7c2
Sergey Antonov <sergant_@mail.ru>
2011-04-05 23:39:22 +0400

    Started work on options infrastructure: - Added options page interface and generic options window - Implemented diff tools page

--- HgSccHelper/Cfg/DiffOptionsWindow.xaml.cs
+++ HgSccHelper/Cfg/DiffOptionsWindow.xaml.cs
@@ -10,11 +10,7 @@
 // 
 //=========================================================================
 
-using System;
-using System.Collections.Generic;
 using System.Windows;
-using System.IO;
-using Path = System.IO.Path;
 
 namespace HgSccHelper
 {
@@ -23,117 +19,19 @@ namespace HgSccHelper
 	/// </summary>
 	public partial class DiffOptionsWindow : Window
 	{
-		private List<MergeToolInfo> merge_tools;
-
 		//------------------------------------------------------------------
 		public DiffOptionsWindow()
 		{
 			InitializeComponent();
-
-			comboTool.Items.Clear();
-
-			merge_tools = new HgMergeTools().GetMergeTools();
-			foreach (var tool in merge_tools)
-			{
-				comboTool.Items.Add(new DiffComboItem
-				{
-					DiffTool = tool.ExecutableFilename,
-					DiffArgs = tool.DiffArgs
-				});
-			}
-
-			if (HgSccOptions.Options.DiffTool.Length != 0)
-				AddDiffTool(HgSccOptions.Options.DiffTool, HgSccOptions.Options.DiffArgs);
-
-			if (comboTool.SelectedIndex == -1 && comboTool.Items.Count > 0)
-				comboTool.SelectedIndex = 0;
-		}
-
-		//------------------------------------------------------------------
-		private void Browse_Click(object sender, RoutedEventArgs e)
-		{
-			string diff_tool = comboTool.Text;
-			if (HgOptionsHelper.BrowseDiffTool(ref diff_tool))
-			{
-				AddDiffTool(diff_tool, "");
-			}
-		}
-
-		//-----------------------------------------------------------------------------
-		private void AddDiffTool(string diff_tool, string diff_args)
-		{
-			var new_tool = merge_tools.Find(tool =>
-											  String.Compare(tool.ExecutableFilename, diff_tool, true) == 0);
-
-			if (new_tool == null)
-			{
-				new_tool = new MergeToolInfo(Path.GetFileNameWithoutExtension(diff_tool));
-				new_tool.Executable = diff_tool;
-				if (!string.IsNullOrEmpty(diff_args))
-					new_tool.DiffArgs = diff_args;
-
-				if (!new_tool.FindExecutable())
-					return;
-			}
-
-			foreach (DiffComboItem item in comboTool.Items)
-			{
-				if (String.Compare(item.DiffTool, new_tool.ExecutableFilename, true) == 0)
-				{
-					if (!string.IsNullOrEmpty(diff_args))
-						item.DiffArgs = diff_args;
-					else
-						item.DiffArgs = new_tool.DiffArgs;
-
-					comboTool.SelectedItem = item;
-					textArgs.Text = item.DiffArgs;
-					return;
-				}
-			}
-
-			comboTool.Items.Add(new DiffComboItem { DiffTool = new_tool.ExecutableFilename, DiffArgs = new_tool.DiffArgs });
-			comboTool.SelectedIndex = comboTool.Items.Count - 1;
-		}
-
-		//------------------------------------------------------------------
-		public string DiffToolPath
-		{
-			get { return comboTool.Text; }
-		}
-
-		//------------------------------------------------------------------
-		public string DiffToolArgs
-		{
-			get { return textArgs.Text; }
 		}
 
 		//------------------------------------------------------------------
 		private void btnOK_Click(object sender, RoutedEventArgs e)
 		{
-			var diff_tool = DiffToolPath;
-
-			if (diff_tool.Length != 0)
-			{
-				if (!File.Exists(diff_tool))
-				{
-					MessageBox.Show("File: " + diff_tool + " is not exist", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-					return;
-				}
-			}
-
-			HgSccOptions.Options.DiffTool = diff_tool;
-			HgSccOptions.Options.DiffArgs = DiffToolArgs;
-			HgSccOptions.Save();
+			if (!diffTools.Save())
+				return;
 
 			DialogResult = true;
-		}
-
-		//-----------------------------------------------------------------------------
-		private void comboTool_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-		{
-			var item = comboTool.SelectedItem as DiffComboItem;
-			if (item != null)
-				textArgs.Text = item.DiffArgs;
 		}
 	}
 }
---
+++ HgSccHelper/Cfg/OptionsWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿//=========================================================================
+// Copyright 2011 Sergey Antonov <sergant_@mail.ru>
+// 
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2 as published by the Free Software
+// Foundation.
+// 
+// See the file COPYING.TXT for the full text of the license, or see
+// http://www.gnu.org/licenses/gpl-2.0.txt
+// 
+//=========================================================================
+
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace HgSccHelper
+{
+	/// <summary>
+	/// Interaction logic for OptionsWindow.xaml
+	/// </summary>
+	public partial class OptionsWindow : Window
+	{
+		private ObservableCollection<IOptionsPage> pages;
+
+		//-----------------------------------------------------------------------------
+		public OptionsWindow()
+		{
+			InitializeComponent();
+			pages = new ObservableCollection<IOptionsPage>();
+
+			this.DataContext = pages;
+		}
+
+		//-----------------------------------------------------------------------------
+		public void AddPage(IOptionsPage page)
+		{
+			pages.Add(page);
+		}
+
+		//-----------------------------------------------------------------------------
+		private void Button_Save(object sender, RoutedEventArgs e)
+		{
+			foreach (var page in pages)
+			{
+				if (!page.Save())
+				{
+					listPages.SelectedItem = page;
+					return;
+				}
+			}
+
+			DialogResult = true;
+		}
+	}
+}
