05023cae15685937df362bfb373941e298c6843c
Sergey Antonov <sergant_@mail.ru>
2011-04-06 20:22:59 +0400

    Added missing footer

--- HgSccPackage/UI/OptionsPageAbout.xaml.cs
+++ HgSccPackage/UI/OptionsPageAbout.xaml.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
+﻿//=========================================================================
+// Copyright 2011 Sergey Antonov <sergant_@mail.ru>
+// 
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2 as published by the Free Software
+// Foundation.
+// 
+// See the file COPYING.TXT for the full text of the license, or see
+// http://www.gnu.org/licenses/gpl-2.0.txt
+// 
+//=========================================================================
+
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using HgSccHelper;
 
 namespace HgSccPackage.UI
