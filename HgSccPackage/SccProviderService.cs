d147ed5c160bebee0a3252d937bfabc86dff61d0
Sergey Antonov <sergant_@mail.ru>
2012-02-05 16:42:35 +0400

    Command server protocol was introduced in 1.9, so required mercurial version is changed to 1.9

--- HgSccPackage/SccProviderService.cs
+++ HgSccPackage/SccProviderService.cs
@@ -294,7 +294,7 @@ namespace HgSccPackage
 			Logger.WriteLine("Provider set active");
 			Logger.WriteLine("sln = '{0}'", _sccProvider.GetSolutionFileName());
 
-			var required_version = new HgVersionInfo {Release = 1, Major = 8};
+			var required_version = new HgVersionInfo {Release = 1, Major = 9};
 			if (!HgVersionChecker.CheckVersion(required_version))
 				return VSConstants.E_FAIL;
 
